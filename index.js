6a7aa55e9f073b4585cfc3e95768fff6549bae68
Ebraheem Fataar <ebraheemfataar@gmail.com>
2024-06-03 22:38:21 +0200

    Added initial Javascript

---
+++ index.js
@@ -0,0 +1,26 @@
+/*=========== SIDEBAR ===========*/
+const menuItems = document.querySelectorAll(".menu-item");
+console.log(menuItems);
+// remove active class from all menu items
+const changeActiveItem = () => {
+  menuItems.forEach((item) => {
+    console.log(item);
+    item.classList.remove("active");
+  });
+};
+
+menuItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    changeActiveItem();
+    item.classList.add("active");
+    if (item.id != "notifications") {
+      document.querySelector(".notifications-popup").style.display = "none";
+    } else {
+      document.querySelector(".notifications-popup").style.display = "block";
+      document.querySelector(
+        "#notifications .notification-count"
+      ).style.display = "none";
+    }
+  });
+});
+/*=========== SIDEBAR ===========*/
